aedfd3a4047a82b18e76db4754b791f478b8f8e2
Crowdbotics <team@crowdbotics.com>
2021-09-27 20:06:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/coinwheels_30904/MainActivity.java
@@ -0,0 +1,15 @@
+package com.coinwheels_30904;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "coinwheels_30904";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "coinwheels_30904",
+  url: "https://coinwheels_30904.botics.co",
+  api: "https://coinwheels_30904.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
